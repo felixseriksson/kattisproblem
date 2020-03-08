06e089fdf168ca0d9cff8bceaacc2874bef5a529
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-08 19:38:03 +0100

    some testing to learn vim

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 lista = str(input()).split()
 
 for n in range(1,int(lista[2])+1):
-    if n % int(lista[0]) == 0 and n % int(lista[1]) == 0:
+    if n % int(lista[1]) == 0 and n % int(lista[1]) == 0:
         print("FizzBuzz")
     elif n % int(lista[0]) == 0:
         print("Fizz")
