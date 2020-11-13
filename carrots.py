c7edea83c5a869fce0b9740cb794dd117e72a086
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-11-13 20:22:29 +0100

    idk

---
+++ carrots.py
@@ -0,0 +1,7 @@
+cases, hufflepuff = [int(x) for x in input().split()]
+for k in range(cases):
+    _ = input()
+print(hufflepuff)
+
+# ((lambda x, y: (lambda z: print(x.split()[1])(int(w) for w in range(x)))([int(x) for x in input().split()])
+# (lambda x, y: True, True)([int(x) for x in input().split()])
